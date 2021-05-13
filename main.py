dc1b24896b3da2fd53c1a34ae9a710d84c8e4579
Spartanjackwar <58582942+Spartanjackwar@users.noreply.github.com>
2021-05-12 20:24:53 -0700

    Started the main file

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+(a) The mean, median, and standard deviation
+
+(b) Draw boxplots of attributes in one frame.
+
+(c) Draw pairplots.  For example, you can use   import seaborn as sns;    and then use  sns.pairplot()
+
+(d) If the scatter plot shows a correlation among variables, then calculate the correlation.  We can use the following command for this purpose.
+
+# calculate Pearson's correlation
+from scipy.stats import pearsonr
+# calculate Pearson's correlation between price and longitude 
+corr, _ = pearsonr(X, y)
+print('Pearsons correlation: %.3f' % corr)
+"""
